8ecdfe67c3efab5710cda87a0703d63c7f956468
Saul Dominguez <saulydominguez@gmail.com>
2025-09-16 13:49:05 +0200

    fix: apply code suggestions

--- packages/react/src/experimental/OneDataCollection/useEventEmitter.ts
+++ packages/react/src/experimental/OneDataCollection/useEventEmitter.ts
@@ -35,17 +35,23 @@ export const useEventEmitter = <Sortings extends SortingsDefinition>({
 
   const emitFilterChange = useCallback(
     (filters: FiltersState<FiltersDefinition>) => {
-      const newFilter = Object.entries(filters ?? {}).find(
+      if (!filters) return
+
+      const changedFilter = Object.entries(filters).find(
         ([field, value]) => latestFilters.current?.[field] !== value
       )
 
-      if (!newFilter || !isScalar(newFilter[1])) return
+      if (!changedFilter) return
+
+      const [field, value] = changedFilter
+
+      if (!isScalar(value)) return
 
       latestFilters.current = filters
 
       onEvent("datacollection.filter-change", {
-        name: newFilter?.[0],
-        value: newFilter?.[1],
+        name: field,
+        value: value,
       })
     },
     [onEvent]
@@ -73,17 +79,23 @@ export const useEventEmitter = <Sortings extends SortingsDefinition>({
 
   const emitPresetClick = useCallback(
     (filters: FiltersState<FiltersDefinition>) => {
-      const newFilter = Object.entries(filters ?? {}).find(
+      if (!filters) return
+
+      const changedFilter = Object.entries(filters).find(
         ([field, value]) => latestFilters.current?.[field] !== value
       )
 
-      if (!newFilter || !isScalar(newFilter[1])) return
+      if (!changedFilter) return
+
+      const [field, value] = changedFilter
+
+      if (!isScalar(value)) return
 
       latestFilters.current = filters
 
       onEvent("datacollection.preset-click", {
-        name: newFilter?.[0],
-        value: newFilter?.[1],
+        name: field,
+        value: value,
       })
     },
     [onEvent]
