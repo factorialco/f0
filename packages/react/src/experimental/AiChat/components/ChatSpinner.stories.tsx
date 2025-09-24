5c0cfa8f0226ee52a869ac4e412e53a48020ee8c
Dani Moreno <hi@dazlog.com>
2025-09-24 16:58:22 +0200

    feat: simple logo

---
+++ packages/react/src/experimental/AiChat/components/ChatSpinner.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react-vite"
+import ChatSpinner from "./ChatSpinner"
+
+const meta: Meta<typeof ChatSpinner> = {
+  title: "Experimental/AiChat/ChatSpinner",
+  component: ChatSpinner,
+  parameters: {
+    layout: "centered",
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    className: "w-5 h-5",
+  },
+}
