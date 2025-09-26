5a70bb49356d701615836b931c4e33bab7cc4b33
Nik Lopin <nik.lopin@factorial.co>
2025-09-26 14:07:20 +0200

    feat: AiChat improvements (#2687)
    * fix: show part of previous message in the chat
    
    * fix: add translations for the message form
    
    * feat: add thinking component
    
    * refactor: simplify header component
    
    * test: add tests for conversion from messages to turns
    
    * refactor: move unimportant things below
    
    * fix: remove error handling
    
    * chore: allow console.assert
    
    * fix: hoist agent state message on top of thinking group
    
    * fix: render chat only when it's enabled
    
    * fix: do not print error for missing context

--- packages/react/src/experimental/AiChat/ActionItem.tsx
+++ packages/react/src/experimental/AiChat/ActionItem.tsx
@@ -1,24 +1,58 @@
 import { F0Icon } from "@/components/F0Icon"
 import CheckCircle from "@/icons/animated/CheckCircle"
 import DottedCircle from "@/icons/app/DottedCircle"
+import { cn } from "@/lib/utils"
+import { motion } from "motion/react"
 import { Spinner } from "../Information/Spinner"
 
 export interface ActionItemProps {
   title: string
-  status: "inProgress" | "executing" | "completed"
+  status?: "inProgress" | "executing" | "completed"
+  inGroup?: boolean
 }
 
-export const ActionItem = ({ title, status }: ActionItemProps) => {
+export const ActionItem = ({ title, status, inGroup }: ActionItemProps) => {
   return (
-    <div className="flex items-center gap-1 text-f1-foreground-secondary">
-      {status === "inProgress" && (
-        <F0Icon state="animate" size="lg" icon={DottedCircle} />
+    <div
+      className={cn(
+        "flex w-full gap-1 text-f1-foreground-secondary",
+        inGroup ? "items-start" : "items-center"
       )}
-      {status === "executing" && <Spinner className="h-6 w-6" />}
-      {status === "completed" && (
-        <F0Icon state="animate" size="lg" icon={CheckCircle} />
-      )}
-      <p>{title}</p>
+    >
+      <div className="*:block">
+        {status === "inProgress" && (
+          <F0Icon
+            state="animate"
+            size={inGroup ? "md" : "lg"}
+            icon={DottedCircle}
+          />
+        )}
+        {status === "executing" && (
+          <Spinner className={inGroup ? "h-4 w-4" : "h-6 w-6"} />
+        )}
+        {status === "completed" && (
+          <F0Icon
+            state="animate"
+            size={inGroup ? "md" : "lg"}
+            icon={CheckCircle}
+          />
+        )}
+      </div>
+      <motion.p
+        key={title}
+        initial={{ opacity: 0, x: 100 }}
+        animate={{
+          opacity: 1,
+          x: 0,
+        }}
+        transition={{
+          ease: "easeOut",
+          duration: 0.3,
+        }}
+        className="text-pretty"
+      >
+        {title}
+      </motion.p>
     </div>
   )
 }
