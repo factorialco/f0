c027d1979b97aa921dcb721e8c396afedc91578b
Sergio Carracedo Martinez <sergio.carracedo@factorial.co>
2025-09-26 19:23:21 +0200

    feat: responsive twocols layout and sticky (#2700)
    * feat: responsive twocols layout and sticky
    
    * feat: main always first focusable even if its on left

--- packages/react/src/components/layouts/TwoColumnLayout/__stories__/TwoColumnLayout.stories.tsx
+++ packages/react/src/components/layouts/TwoColumnLayout/__stories__/TwoColumnLayout.stories.tsx
@@ -6,8 +6,22 @@ import { Dashboard } from "@/experimental/Widgets/Layout/Dashboard"
 import * as DashboardStories from "@/experimental/Widgets/Layout/Dashboard/index.stories"
 import { PageDecorator } from "@/lib/storybook-utils/pageDecorator"
 import { ComponentProps } from "react"
-import { TwoColumnLayout } from "./index"
+import { TwoColumnLayout } from "../index"
 
+const FocusableElement = () => {
+  return (
+    <div className="p-4">
+      <label htmlFor="focusable-element">Focusable Element</label>
+      <div>
+        <input
+          type="text"
+          className="border-0 bg-[#fff] px-2"
+          id="focusable-element"
+        />
+      </div>
+    </div>
+  )
+}
 const meta = {
   title: "Layout/TwoColumnLayout",
   component: TwoColumnLayout,
@@ -15,13 +29,18 @@ const meta = {
   decorators: [PageDecorator],
   args: {
     children: (
-      <div className="flex h-64 items-center justify-center bg-f1-foreground-info text-f1-foreground-inverse">
+      <div className="flex h-64 flex-col items-center justify-center bg-f1-foreground-info text-f1-foreground-inverse">
         Main
+        <FocusableElement />
+        <p>
+          Should the the first focusable element even if main is on right side
+        </p>
       </div>
     ),
     sideContent: (
-      <div className="flex h-48 items-center justify-center bg-f1-foreground-secondary text-f1-foreground-inverse">
+      <div className="flex h-48 flex-col items-center justify-center bg-f1-foreground-secondary text-f1-foreground-inverse">
         Side
+        <FocusableElement />
       </div>
     ),
   },
@@ -29,6 +48,16 @@ const meta = {
     a11y: {
       config: {
         rules: [{ id: "svg-img-alt", enabled: false }],
+      },
+    },
+    docs: {
+      description: {
+        component: [
+          "A two column layout component that is used to display a main content and a side content.",
+          "The order of the content and the aside can be modified",
+        ]
+          .map((item) => <p key={item}>{item}</p>)
+          .join(""),
       },
     },
   },
@@ -41,6 +70,7 @@ export const Default: Story = {}
 
 export const Profile: Story = {
   args: {
+    mainColumnPosition: "left",
     children: <Dashboard {...DashboardStories.default.args} />,
     sideContent: (
       <DetailsItemsList
@@ -49,6 +79,13 @@ export const Profile: Story = {
         >)}
       />
     ),
+  },
+}
+
+export const ProfileWithSticky: Story = {
+  args: {
+    ...Profile.args,
+    sticky: true,
   },
 }
 
