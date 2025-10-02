c827afeedfb5e1ac85a2b40205af7cec29a591e5
Josiane Olatoundji <josianeolatoundji@gmail.com>
2025-10-02 12:41:27 +0100

    feat: add positive variant to TagAlert component

--- packages/react/src/components/tags/F0TagAlert/types.ts
+++ packages/react/src/components/tags/F0TagAlert/types.ts
@@ -1,4 +1,4 @@
-export type Level = "info" | "warning" | "critical"
+export type Level = "info" | "warning" | "critical" | "positive"
 
 export type Props<Text extends string = string> = {
   text: Text extends "" ? never : Text
