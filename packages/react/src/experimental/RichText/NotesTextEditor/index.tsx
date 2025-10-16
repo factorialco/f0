bc70f0dc78eef5edaaac9e955a1fce11ceffe796
siguenzaraul <raul.siguenza@factorial.co>
2025-10-16 22:59:45 +0200

    feat(RichTextEditor): integrate ScrollArea for improved toolbar and header layout

--- packages/react/src/experimental/RichText/NotesTextEditor/index.tsx
+++ packages/react/src/experimental/RichText/NotesTextEditor/index.tsx
@@ -229,14 +229,16 @@ const NotesTextEditorComponent = forwardRef<
         (metadata && metadata.length > 0)) && (
         <Header actions={actions} metadata={metadata} />
       )}
-      <div className="absolute bottom-8 left-1/2 z-50 -translate-x-1/2 rounded-lg bg-f1-background p-2 shadow-md">
-        <Toolbar
-          labels={toolbarLabels}
-          editor={editor}
-          disableButtons={false}
-          showEmojiPicker={false}
-          plainHtmlMode={false}
-        />
+      <div className="absolute bottom-8 left-1/2 z-50 max-w-[calc(100%-16px)] -translate-x-1/2 rounded-lg bg-f1-background shadow-md">
+        <ScrollArea className="w-full p-2">
+          <Toolbar
+            labels={toolbarLabels}
+            editor={editor}
+            disableButtons={false}
+            showEmojiPicker={false}
+            plainHtmlMode={false}
+          />
+        </ScrollArea>
       </div>
 
       <ScrollArea className="h-full gap-6">
