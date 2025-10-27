8b748f09e4f945b30a83c2f19500b52439da97f3
Segio Carracedo <sergio.carracedo@factorial.co>
2025-10-27 16:07:29 +0100

    chore: fix avatarlist

--- packages/react/src/components/avatars/F0AvatarList/F0AvatarList.tsx
+++ packages/react/src/components/avatars/F0AvatarList/F0AvatarList.tsx
@@ -2,7 +2,7 @@ import { OverflowList } from "@/ui/OverflowList"
 import { AvatarVariant, F0Avatar } from "../F0Avatar"
 import { MaxCounter } from "./components/MaxCounter"
 
-import { Tooltip } from "@/experimental/Overlays/Tooltip"
+import { Tooltip } from "@/experimental/Overlays/Tooltip/index"
 import { useMemo } from "react"
 import { AvatarListSize, avatarListSizes, F0AvatarListProps } from "./types"
 import { getAvatarDisplayName } from "./utils"
