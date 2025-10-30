44e059252b4e3ebb3b29ddfa3f5ba17be9a83bb2
Dan Petre <83948921+developerdanx@users.noreply.github.com>
2025-10-30 13:24:13 +0100

    feat: restructure exports for AiChat components and types

--- packages/react/src/ai.ts
+++ packages/react/src/ai.ts
@@ -1,1 +1,20 @@
-export * from "./ai/AiChat"
+export { ActionItem, type ActionItemProps } from "./ai/AiChat/ActionItem"
+export type { WelcomeScreenSuggestion } from "./ai/AiChat/components/WelcomeScreen"
+export {
+  HILActionConfirmation,
+  type HILActionConfirmationProps,
+} from "./ai/AiChat/HILActionConfirmation"
+export {
+  AiChat,
+  AiChatProvider,
+  AiFullscreenChat,
+  type AiChatProviderProps,
+} from "./ai/AiChat/index"
+export { useAiChat } from "./ai/AiChat/providers/AiChatStateProvider"
+export {
+  AiChatTranslationsProvider,
+  aiTranslations,
+  useAiChatTranslations,
+  type AiChatTranslations,
+  type AiChatTranslationsProviderProps,
+} from "./ai/AiChat/providers/AiChatTranslationsProvider"
