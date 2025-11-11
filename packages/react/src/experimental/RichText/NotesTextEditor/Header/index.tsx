d223a6a6f9af6d80745af5635744133dfae520d3
Raúl Sigüenza Sánchez <raul.siguenza@factorial.co>
2025-11-11 17:23:46 +0100

    feat(RichTextEditor): add secondary actions and person metadata support (#2940)

--- packages/react/src/experimental/RichText/NotesTextEditor/Header/index.tsx
+++ packages/react/src/experimental/RichText/NotesTextEditor/Header/index.tsx
@@ -1,14 +1,17 @@
+import { F0Avatar } from "@/components/avatars/F0Avatar"
 import { F0Button } from "@/components/F0Button"
 import { F0TagDot } from "@/components/tags/F0TagDot"
 import { F0TagRaw } from "@/components/tags/F0TagRaw"
 import { F0TagStatus } from "@/components/tags/F0TagStatus"
+import { Dropdown } from "@/experimental/Navigation/Dropdown"
 import { ScrollArea } from "@/experimental/Utilities/ScrollArea"
 import { Fragment } from "react"
-import { actionType, MetadataItemValue } from "../types"
+import { actionType, MetadataItemValue, secondaryActionsType } from "../types"
 
 interface HeaderProps {
   actions?: actionType[]
   metadata?: MetadataItemValue[]
+  secondaryActions?: secondaryActionsType[]
 }
 
 const buildMetadataItems = ({ items }: { items: MetadataItemValue[] }) =>
@@ -20,6 +23,24 @@ const buildMetadataItems = ({ items }: { items: MetadataItemValue[] }) =>
         <F0TagDot text={item.label} color={item.color} />
       ) : item.type === "tag" ? (
         <F0TagRaw text={item.label} icon={item.icon} />
+      ) : item.type === "person" ? (
+        <div className="flex items-center gap-1">
+          <div className="flex w-fit items-center truncate text-f1-foreground-secondary">
+            <span>{item.label}</span>
+          </div>
+          <F0Avatar
+            avatar={{
+              type: "person",
+              firstName: item.firstName,
+              lastName: item.lastName,
+              src: item.src,
+            }}
+            size="xs"
+          />
+          <span>
+            {item.firstName} {item.lastName}
+          </span>
+        </div>
       ) : (
         item.type === "text" && (
           <div className="flex flex-row gap-2">
@@ -36,7 +57,7 @@ const buildMetadataItems = ({ items }: { items: MetadataItemValue[] }) =>
     </Fragment>
   ))
 
-const Header = ({ actions, metadata }: HeaderProps) => {
+const Header = ({ actions, metadata, secondaryActions }: HeaderProps) => {
   return (
     <div className="flex items-center justify-between gap-2 px-6 py-3">
       <ScrollArea>
@@ -45,6 +66,16 @@ const Header = ({ actions, metadata }: HeaderProps) => {
         </div>
       </ScrollArea>
       <div className="flex flex-shrink-0 flex-row gap-2">
+        {secondaryActions && (
+          <Dropdown
+            items={secondaryActions.map((action) => ({
+              label: action.label,
+              icon: action.icon,
+              onClick: action.onClick,
+              critical: action.critical,
+            }))}
+          />
+        )}
         {actions?.map((action, index) => (
           <F0Button
             key={index}
