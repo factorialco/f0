de14dc73985922d778ba56f94a030e839a000aac
Segio Carracedo <sergio.carracedo@factorial.co>
2025-11-17 16:59:07 +0100

    chore: wip

--- packages/react/src/components/layouts/page/PageLayout/index.ts
+++ packages/react/src/components/layouts/page/PageLayout/index.ts
@@ -1,8 +1,8 @@
 import { PageLayout as PageLayoutComponent } from "./PageLayout"
+import { PageLayoutGroup } from "./components/LayoutGroup/PageLayoutGroup"
 import { PageLayoutBlock } from "./components/PageLayoutBlock/PageLayoutBlock"
-import { PageLayoutGroup } from "./components/PageLayoutGroup/PageLayoutGroup"
+export * from "./components/LayoutGroup/types"
 export * from "./components/PageLayoutBlock/types"
-export * from "./components/PageLayoutGroup/types"
 
 export const PageLayout = Object.assign(PageLayoutComponent, {
   Block: PageLayoutBlock,
