03fb6f076e7fde2d5734b8f8a45a6b4a91b1a970
Dani Moreno <hi@dazlog.com>
2025-12-03 12:36:42 +0100

    fix: widget content scrolling

--- packages/react/src/components/F0Widget/F0Widget.tsx
+++ packages/react/src/components/F0Widget/F0Widget.tsx
@@ -137,7 +137,9 @@ const F0WidgetBase = ({
           </div>
         )}
       </div>
-      <div className="flex flex-1 flex-col px-4 pb-4">{children}</div>
+      <div className="flex max-h-full flex-1 flex-col overflow-y-auto px-4 pb-4">
+        {children}
+      </div>
     </div>
   )
 }
