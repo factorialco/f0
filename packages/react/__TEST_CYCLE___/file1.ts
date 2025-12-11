2eb56124420d3f9167cb9a135ca3d208429effc2
Segio Carracedo <sergio.carracedo@factorial.co>
2025-12-11 09:35:04 +0100

    chore: test cycle hook

--- packages/react/__TEST_CYCLE___/file1.ts
+++
@@ -1,7 +0,0 @@
-import { file2 } from "./file2"
-
-export const file1 = "file1"
-
-export const printValue = () => {
-  console.log(file2)
-}
--- packages/react/__TEST_CYCLE___/file2.ts
+++
@@ -1,7 +0,0 @@
-import { file1 } from "./file1"
-
-export const file2 = "file2"
-
-export const printValue = () => {
-  console.log(file1)
-}
