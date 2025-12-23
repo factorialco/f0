8afb83226f47ce6c5648365934f1e4efccd81eb3
siguenzaraul <raul.siguenza@factorial.co>
2025-12-23 13:29:24 +0100

    feat: add pageOnlySelection feature to ExampleComponent and useSelectable hook

--- packages/react/src/experimental/OneDataCollection/__stories__/mockData.tsx
+++ packages/react/src/experimental/OneDataCollection/__stories__/mockData.tsx
@@ -1047,6 +1047,7 @@ export const ExampleComponent = ({
   frozenColumns = 0,
   selectable,
   defaultSelectedItems,
+  pageOnlySelection,
   bulkActions,
   currentGrouping,
   currentSortings,
@@ -1107,6 +1108,7 @@ export const ExampleComponent = ({
   >
   defaultSelectedItems?: SelectedItemsState<MockUser>
   selectable?: (item: MockUser) => string | number | undefined
+  pageOnlySelection?: boolean
   bulkActions?: BulkActionsDefinition<MockUser, FiltersType>
   onSelectItems?: OnSelectItemsCallback<MockUser, FiltersType>
   onBulkAction?: OnBulkActionCallback<MockUser, FiltersType>
@@ -1232,6 +1234,7 @@ export const ExampleComponent = ({
       ],
       selectable,
       defaultSelectedItems,
+      pageOnlySelection,
       bulkActions,
       totalItemSummary,
       search:
