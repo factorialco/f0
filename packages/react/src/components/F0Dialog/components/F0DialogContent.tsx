5a4e61cf78b831b4026827345e07f7cc0c5f8ce2
siguenzaraul <raul.siguenza@factorial.co>
2026-01-12 11:07:37 +0100

    fix(F0DialogContent): streamline conditional class application for fullscreen position

--- packages/react/src/components/F0Dialog/components/F0DialogContent.tsx
+++ packages/react/src/components/F0Dialog/components/F0DialogContent.tsx
@@ -23,8 +23,7 @@ export const F0DialogContent = ({
         "[*[data-state=visible]_div]:bg-f1-background flex flex-1 flex-col",
         "[&_.resource-header]:p-0 [&_.resource-header]:pr-1",
         !disableContentPadding && "px-4 [&>div]:py-4",
-        position === "fullscreen" &&
-          "h-full [&>div]:h-full [&>div>div]:h-full"
+        position === "fullscreen" && "h-full [&>div]:h-full [&>div>div]:h-full"
       )}
     >
       {children}
