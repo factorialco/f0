8f0a9bfd3198bfd566c0299399d4d6fa8e803385
Dan Petre <83948921+developerdanx@users.noreply.github.com>
2026-01-19 17:13:08 +0100

    chore: small commit to trigger an anplah release

--- packages/react/src/ai/AiChat/index.tsx
+++ packages/react/src/ai/AiChat/index.tsx
@@ -245,6 +245,8 @@ const AiChatCmp = () => {
   )
 }
 
+// fake comment to triger an alpha release
+
 const AiFullscreenChatCmp = () => {
   const { enabled } = useAiChat()
   const [inProgress, setInProgress] = useState(false)
