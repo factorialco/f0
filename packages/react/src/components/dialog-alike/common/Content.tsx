8cc14de219708bb6a2fa67e255bb443577b670ef
Segio Carracedo <sergio.carracedo@factorial.co>
2026-01-26 15:21:12 +0100

    chore: merge main

--- packages/react/src/components/dialog-alike/common/Content.tsx
+++ packages/react/src/components/dialog-alike/common/Content.tsx
@@ -1,8 +1,6 @@
 import { cn } from "@/lib/utils"
 import { ScrollArea, ScrollBar } from "@/ui/scrollarea"
 
-import { useDialogWrapperContext } from "./DialogWrapperProvider"
-
 export type ContentProps = {
   children: React.ReactNode
   /**
@@ -16,15 +14,12 @@ export const Content = ({
   children,
   disableContentPadding = false,
 }: ContentProps) => {
-  const { position } = useDialogWrapperContext()
-
   return (
     <ScrollArea
       className={cn(
         "[*[data-state=visible]_div]:bg-f1-background flex flex-1 flex-col",
         "[&_.resource-header]:p-0 [&_.resource-header]:pr-1",
-        !disableContentPadding && "px-4 [&>div]:py-4",
-        position === "fullscreen" && "h-full [&>div]:h-full [&>div>div]:h-full"
+        !disableContentPadding && "px-4 [&>div]:py-4"
       )}
     >
       {children}
