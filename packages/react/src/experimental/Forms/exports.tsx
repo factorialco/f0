53579a12aa18cf451278a871759de4b7d6d805b4
Saul Dominguez <saulydominguez@gmail.com>
2026-02-02 14:49:12 +0100

    fix: change location of F0Form

--- packages/react/src/experimental/Forms/exports.tsx
+++ packages/react/src/experimental/Forms/exports.tsx
@@ -1,6 +1,5 @@
 export * from "./CardSelectable"
 export * from "./EntitySelect/exports"
-export * from "./F0Form"
 export * from "./Fields/exports"
 export * from "./Form"
 export * from "./FormField"
