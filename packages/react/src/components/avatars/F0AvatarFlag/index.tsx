2f2483d30ccbe737fab85690174e0826121a608c
Segio Carracedo <sergio.carracedo@factorial.co>
2026-02-03 11:46:01 +0100

    chore: datatestid testing strategy

--- packages/react/src/components/avatars/F0AvatarFlag/index.tsx
+++ packages/react/src/components/avatars/F0AvatarFlag/index.tsx
@@ -1,6 +1,6 @@
 import { withDataTestId } from "@/lib/data-testid/index.tsx"
 
-export { F0AvatarFlag as _F0AvatarFlag } from "./F0AvatarFlag.tsx"
+import { F0AvatarFlag as _F0AvatarFlag } from "./F0AvatarFlag.tsx"
 export type { F0AvatarFlagProps } from "./types"
 
 export const F0AvatarFlag = withDataTestId(_F0AvatarFlag)
