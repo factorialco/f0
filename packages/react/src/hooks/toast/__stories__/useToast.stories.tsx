a18abcbacef7da19f3d6982594604c35a9ecaad8
Segio Carracedo <sergio.carracedo@factorial.co>
2026-02-03 09:27:22 +0100

    chore: right-top position

--- packages/react/src/hooks/toast/__stories__/useToast.stories.tsx
+++ packages/react/src/hooks/toast/__stories__/useToast.stories.tsx
@@ -35,6 +35,10 @@ export const ToastPlayground: Story = {
     const { toast, removeToast } = useToast()
     const [lastId, setLastId] = useState<ToastId[]>([])
 
+    const [logger, setLogger] = useState<{ date: string; message: string }[]>(
+      []
+    )
+
     const longDescription = `This is a long description that demonstrates how the toast handles extended text content and wraps appropriately within the available space.
     
     This is a long description that demonstrates how the toast handles extended text content and wraps appropriately within the available space.
@@ -54,7 +58,12 @@ export const ToastPlayground: Story = {
           {
             type: "button",
             label: "Undo",
-            onClick: () => alert("Undo clicked"),
+            onClick: () => {
+              setLogger((prev) => [
+                ...prev,
+                { date: new Date().toISOString(), message: "Undo clicked" },
+              ])
+            },
           },
           {
             type: "link",
@@ -71,7 +80,12 @@ export const ToastPlayground: Story = {
           {
             type: "button",
             label: "View",
-            onClick: () => alert("View clicked"),
+            onClick: () => {
+              setLogger((prev) => [
+                ...prev,
+                { date: new Date().toISOString(), message: "View clicked" },
+              ])
+            },
           },
         ],
       },
@@ -83,7 +97,12 @@ export const ToastPlayground: Story = {
           {
             type: "button",
             label: "Review",
-            onClick: () => alert("Review clicked"),
+            onClick: () => {
+              setLogger((prev) => [
+                ...prev,
+                { date: new Date().toISOString(), message: "Review clicked" },
+              ])
+            },
           },
         ],
       },
@@ -95,7 +114,12 @@ export const ToastPlayground: Story = {
           {
             type: "button",
             label: "Retry",
-            onClick: () => alert("Retry clicked"),
+            onClick: () => {
+              setLogger((prev) => [
+                ...prev,
+                { date: new Date().toISOString(), message: "Retry clicked" },
+              ])
+            },
           },
           {
             type: "link",
@@ -108,108 +132,134 @@ export const ToastPlayground: Story = {
 
     const newToast = (options: ToastOptions) => {
       return () => {
+        setLogger((prev) => [
+          ...prev,
+          {
+            date: new Date().toISOString(),
+            message: `Toast ${options.title} created`,
+          },
+        ])
         toast(options)
       }
     }
 
     return (
-      <div className="flex flex-col gap-8 p-4">
-        <div className="flex flex-col gap-4">
-          <h3 className="font-bold text-lg">Variants</h3>
-          <div className="flex flex-wrap gap-2">
-            {variants.map((variant) => (
-              <F0Button
-                key={variant.label}
-                label={variant.label}
-                onClick={newToast({
-                  variant: variant.variant,
-                  title: variant.label,
-                })}
-              />
-            ))}
+      <div className="flex flex-row gap-10">
+        <div className="order-2 flex w-[300px] flex-col gap-4 p-4">
+          <h3 className="font-bold text-lg">Logger</h3>
+          <div className="flex max-h-[500px] flex-col gap-2 overflow-y-auto">
+            {logger
+              .slice()
+              .reverse()
+              .map((item) => (
+                <div key={item.date}>
+                  <p>
+                    {item.date} {item.message}
+                  </p>
+                </div>
+              ))}
           </div>
         </div>
-
-        <div className="flex flex-col gap-4">
-          <h3 className="font-bold text-lg">Variants with Actions</h3>
-          <div className="flex flex-wrap gap-2">
-            {variants.map((variant) => (
-              <F0Button
-                key={variant.label}
-                label={variant.label}
-                onClick={newToast({
-                  variant: variant.variant,
-                  title: variant.label,
-                  actions: variant.actions,
-                })}
-              />
-            ))}
+        <div className="order-1 flex flex-col gap-8 p-4">
+          <div className="flex flex-col gap-4">
+            <h3 className="font-bold text-lg">Variants</h3>
+            <div className="flex flex-wrap gap-2">
+              {variants.map((variant) => (
+                <F0Button
+                  key={variant.label}
+                  label={variant.label}
+                  onClick={newToast({
+                    variant: variant.variant,
+                    title: variant.label,
+                  })}
+                />
+              ))}
+            </div>
           </div>
-        </div>
 
-        <div className="flex flex-col gap-4">
-          <h3 className="font-bold text-lg">Variants with Description</h3>
-          <div className="flex flex-wrap gap-2">
-            {variants.map((variant) => (
-              <F0Button
-                key={variant.label}
-                label={variant.label}
-                onClick={newToast({
-                  variant: variant.variant,
-                  description: variant.description,
-                  title: variant.label,
-                })}
-              />
-            ))}
+          <div className="flex flex-col gap-4">
+            <h3 className="font-bold text-lg">Variants with Actions</h3>
+            <div className="flex flex-wrap gap-2">
+              {variants.map((variant) => (
+                <F0Button
+                  key={variant.label}
+                  label={variant.label}
+                  onClick={newToast({
+                    variant: variant.variant,
+                    title: variant.label,
+                    actions: variant.actions,
+                  })}
+                />
+              ))}
+            </div>
           </div>
-        </div>
 
-        <div className="flex flex-col gap-4">
-          <h3 className="font-bold text-lg">Variants with Long Description</h3>
-          <div className="flex flex-wrap gap-2">
-            {variants.map((variant) => (
-              <F0Button
-                key={variant.label}
-                label={variant.label}
-                onClick={newToast({
-                  variant: variant.variant,
-                  description: longDescription,
-                  title: variant.label,
-                })}
-              />
-            ))}
+          <div className="flex flex-col gap-4">
+            <h3 className="font-bold text-lg">Variants with Description</h3>
+            <div className="flex flex-wrap gap-2">
+              {variants.map((variant) => (
+                <F0Button
+                  key={variant.label}
+                  label={variant.label}
+                  onClick={newToast({
+                    variant: variant.variant,
+                    description: variant.description,
+                    title: variant.label,
+                  })}
+                />
+              ))}
+            </div>
           </div>
-        </div>
 
-        <div className="flex flex-col gap-4">
-          <h3 className="font-bold text-lg">Programmatic Control</h3>
-          <div className="flex flex-wrap gap-2">
-            <F0Button
-              label="Create Persistent"
-              onClick={() => {
-                const id = toast({
-                  title: "Persistent Toast",
-                  description: "Click the remove button to close me",
-                  persistent: true,
-                  variant:
-                    toastVariants[
-                      Math.floor(Math.random() * toastVariants.length)
-                    ],
-                })
-                setLastId((prev) => [...prev, id])
-              }}
-            />
-            <F0Button
-              label="Remove Last"
-              disabled={lastId.length === 0}
-              onClick={() => {
-                if (lastId.length > 0) {
-                  const idToRemove = lastId[lastId.length - 1]
-                  removeToast(idToRemove)
-                  setLastId((prev) => prev.slice(0, -1))
-                }
-              }}
-            />
+          <div className="flex flex-col gap-4">
+            <h3 className="font-bold text-lg">
+              Variants with Long Description
+            </h3>
+            <div className="flex flex-wrap gap-2">
+              {variants.map((variant) => (
+                <F0Button
+                  key={variant.label}
+                  label={variant.label}
+                  onClick={newToast({
+                    variant: variant.variant,
+                    description: longDescription,
+                    title: variant.label,
+                  })}
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <h3 className="font-bold text-lg">Programmatic Control</h3>
+            <div className="flex flex-wrap gap-2">
+              <F0Button
+                label="Create Persistent"
+                onClick={() => {
+                  const id = toast({
+                    title: "Persistent Toast",
+                    description: "Click the remove button to close me",
+                    persistent: true,
+                    variant:
+                      toastVariants[
+                        Math.floor(Math.random() * toastVariants.length)
+                      ],
+                  })
+                  setLastId((prev) => [...prev, id])
+                }}
+              />
+              <F0Button
+                label="Remove Last"
+                disabled={lastId.length === 0}
+                onClick={() => {
+                  if (lastId.length > 0) {
+                    const idToRemove = lastId[lastId.length - 1]
+                    removeToast(idToRemove)
+                    setLastId((prev) => prev.slice(0, -1))
+                  }
+                }}
+              />
+            </div>
           </div>
         </div>
       </div>
