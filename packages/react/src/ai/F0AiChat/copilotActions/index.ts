4cb4305a810e4b8953623bdfa249152d89eb28cc
Chloe Dumit <chloe.dumit@factorial.co>
2026-02-03 15:37:26 -0300

    feat: new one upselling components

--- packages/react/src/ai/F0AiChat/copilotActions/index.ts
+++ packages/react/src/ai/F0AiChat/copilotActions/index.ts
@@ -1,6 +1,10 @@
 // Action hooks
 export { useOrchestratorThinkingAction } from "./useOrchestratorThinkingAction"
 export { useMessageSourcesAction } from "./useMessageSourcesAction"
+export { useDemoCardAction } from "./useDemoCardAction"
+export { useBookAMeetingCardAction } from "./useBookAMeetingCardAction"
+export { useQuestionCardAction } from "./useQuestionCardAction"
+export { useModuleCardAction } from "./useModuleCardAction"
 
 // Composite hook for all default actions
 export { useDefaultCopilotActions } from "./useDefaultCopilotActions"
