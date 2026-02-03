a0ae52efb1bb3283516ce9ca5e852b0c4cdae5bb
Chloe Dumit <chloe.dumit@factorial.co>
2026-02-03 15:37:26 -0300

    feat: new one upselling components

--- packages/react/src/ai/F0AiChat/copilotActions/useDefaultCopilotActions.ts
+++ packages/react/src/ai/F0AiChat/copilotActions/useDefaultCopilotActions.ts
@@ -1,5 +1,9 @@
+import { useBookAMeetingCardAction } from "./useBookAMeetingCardAction"
+import { useDemoCardAction } from "./useDemoCardAction"
 import { useMessageSourcesAction } from "./useMessageSourcesAction"
+import { useModuleCardAction } from "./useModuleCardAction"
 import { useOrchestratorThinkingAction } from "./useOrchestratorThinkingAction"
+import { useQuestionCardAction } from "./useQuestionCardAction"
 
 /**
  * Hook to register all default copilot actions.
@@ -17,4 +21,8 @@ export const useDefaultCopilotActions = () => {
   // Each hook internally uses useCopilotAction to register the action
   useOrchestratorThinkingAction()
   useMessageSourcesAction()
+  useDemoCardAction()
+  useBookAMeetingCardAction()
+  useQuestionCardAction()
+  useModuleCardAction()
 }
