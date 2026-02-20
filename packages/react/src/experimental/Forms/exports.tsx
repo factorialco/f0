7dfa93228daa30df369b29f962dc2d778c1b47e9
Saul Dominguez <saul.dominguez@factorial.co>
2026-02-20 12:15:54 +0100

    refactor: remove old form component to avoid confusion from devs

--- packages/react/src/experimental/Forms/exports.tsx
+++ packages/react/src/experimental/Forms/exports.tsx
@@ -1,6 +1,3 @@
 export * from "./CardSelectable"
 export * from "./EntitySelect/exports"
 export * from "./Fields/exports"
-export * from "./Form"
-export * from "./FormField"
-export { useForm, useFormSchema } from "./lib/useForm"
