b46c1212a7edee59ad9cdcd5713ca369d0249455
Oscar Blanquez <36045526+oscarbyte@users.noreply.github.com>
2026-02-26 12:36:42 +0100

    Fix formatting in Question.tsx

--- packages/react-native/src/icons/app/Question.tsx
+++ packages/react-native/src/icons/app/Question.tsx
@@ -1,7 +1,6 @@
 import { Ref, forwardRef } from "react"
 import Svg, { Path } from "react-native-svg"
 import type { SvgProps } from "react-native-svg"
-
 const SvgQuestion = (props: SvgProps, ref: Ref<Svg>) => (
   <Svg
     fill="none"
