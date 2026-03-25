56632a5b827d854b74151e64d58b07adbe79728f
Chloe Dumit <chloe.dumit@factorial.co>
2026-03-25 11:47:35 -0300

    fix: adding decision

--- packages/react/src/sds/UpsellingKit/ai/F0MessageCreditsWarning/F0MessageCreditsWarning.tsx
+++ packages/react/src/sds/UpsellingKit/ai/F0MessageCreditsWarning/F0MessageCreditsWarning.tsx
@@ -1,7 +1,7 @@
+import { F0Alert } from "@/components/F0Alert"
 import { useI18n } from "@/lib/providers/i18n"
 
 import { F0MessageCreditsWarningProps } from "./types"
-import { F0Alert } from "@/components/F0Alert"
 
 export const F0MessageCreditsWarning = ({
   actionHref,
@@ -13,15 +13,16 @@ export const F0MessageCreditsWarning = ({
       title={translations?.ai?.creditWarning.messageBanner?.title}
       variant="warning"
       description={translations?.ai?.creditWarning.messageBanner?.description}
-      action={{
-        label: actionHref
-          ? translations?.ai?.creditWarning.messageBanner?.actionLabel
-          : "",
-        onClick: () => {
-          actionHref ? (window.location.href = actionHref) : null
-          return
-        },
-      }}
+      action={
+        actionHref
+          ? {
+              label: translations?.ai?.creditWarning.messageBanner?.actionLabel,
+              onClick: () => {
+                window.location.href = actionHref
+              },
+            }
+          : undefined
+      }
     />
   )
 }
