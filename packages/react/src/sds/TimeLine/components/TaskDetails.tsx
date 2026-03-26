fb0c1adc78ca57b8db4651b73e31c0ad04149898
Tornamorell <51385742+Tornamorell@users.noreply.github.com>
2026-03-26 10:24:19 +0100

    Update packages/react/src/sds/TimeLine/components/TaskDetails.tsx
    Co-authored-by: Saúl Domínguez Gómez <saulydominguez@gmail.com>

--- packages/react/src/sds/TimeLine/components/TaskDetails.tsx
+++ packages/react/src/sds/TimeLine/components/TaskDetails.tsx
@@ -4,8 +4,8 @@ import type { F0TimelineRowTaskProps } from "../types"
 
 import { Actions } from "./Actions"
 
-export const TaskDetails = ({ props }: { props: F0TimelineRowTaskProps }) => {
-  const { metadata, primaryAction, secondaryActions, otherActions } = props
+export const TaskDetails = ({ metadata, primaryAction, secondaryActions, otherActions } { props: F0TimelineRowTaskProps }) => {
+
 
   const hasMetadata = metadata?.some(Boolean)
   const hasActions =
