bdab02b67a58e8128f68f440f943dc8e7c8b0e83
Danilo Pereira <danilo.pereira@factorial.co>
2026-03-31 16:12:02 +0200

    feat: add showSearchBox and searchBoxPlaceholder to dropdown single question types

--- packages/react/src/sds/surveys/SurveyFormBuilder/QuestionTypes/DropdownSingleQuestion/types.ts
+++ packages/react/src/sds/surveys/SurveyFormBuilder/QuestionTypes/DropdownSingleQuestion/types.ts
@@ -1,11 +1,13 @@
-import { BaseQuestionPropsForOtherQuestionComponents } from "../BaseQuestion"
 import { BaseQuestionOnChangeParams, SelectQuestionOption } from "../../types"
+import { BaseQuestionPropsForOtherQuestionComponents } from "../BaseQuestion"
 
 export type DropdownSingleQuestionOnChangeParams =
   BaseQuestionOnChangeParams & {
     type: "dropdown-single"
     options?: SelectQuestionOption[]
     value?: string | null
+    showSearchBox?: boolean
+    searchBoxPlaceholder?: string
   }
 
 export type DropdownSingleQuestionProps =
