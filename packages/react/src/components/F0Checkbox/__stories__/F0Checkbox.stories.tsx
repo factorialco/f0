dec3c3cd58881059be2bd793ddfcfb2de8ada099
Eliseo Juan <eliseo.juan@factorial.co>
2026-04-07 10:16:36 +0200

    refactor(F0Checkbox): promote from experimental to stable

--- packages/react/src/components/F0Checkbox/__stories__/F0Checkbox.stories.tsx
+++ packages/react/src/components/F0Checkbox/__stories__/F0Checkbox.stories.tsx
@@ -9,7 +9,7 @@ import { F0Checkbox } from "../F0Checkbox"
 
 const meta = {
   component: F0Checkbox,
-  tags: ["autodocs", "experimental"],
+  tags: ["autodocs", "stable"],
   title: "Checkbox",
   parameters: {
     layout: "centered",
