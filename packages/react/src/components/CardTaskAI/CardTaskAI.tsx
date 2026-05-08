886f6187790b7f9d025aa92366fef9aa44204334
Pili Gimenez <pili.gimenez@factorial.co>
2026-05-08 12:48:38 +0200

    style: remove all padding from wrapper
    - Remove pb-[12px] px-[16px]
    - Wrapper now has only flex gap-[8px]

--- packages/react/src/components/CardTaskAI/CardTaskAI.tsx
+++ packages/react/src/components/CardTaskAI/CardTaskAI.tsx
@@ -186,7 +186,7 @@ const CardTaskAIBase = forwardRef<HTMLDivElement, CardTaskAIProps>(
           ${className}
         `}
       >
-        <div className="flex gap-[8px] pb-[12px] px-[16px]">
+        <div className="flex gap-[8px]">
           {/* Icon Section */}
           <div className="flex h-[40px] w-[40px] flex-shrink-0 items-center justify-center">
             {icon}
