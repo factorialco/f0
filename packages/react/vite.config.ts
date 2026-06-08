d7d0f97cb571489e9f5deab72807acfd771a126b
Elias Hernandis <elias.hernandis@factorial.co>
2026-06-08 09:14:05 +0200

    chore: separate recharts

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -159,6 +159,7 @@ export default defineConfig({
         /@copilotkit\/.*/,
         /@livekit\/.*/,
         "livekit-client",
+        /^recharts(\/.*)?$/,
       ],
       maxParallelFileOps: 100,
       // Workaround to fix rebuild https://github.com/vitejs/vite/issues/19410#issuecomment-2661835482
