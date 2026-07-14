17729218583e2aec1beae352b338990b94d91161
Zygis Sniurevicius <zygimantas.sniurevicius@factorial.co>
2026-07-14 20:55:44 +0200

    Published branch "feat/analytics-dashboard-item-filters" (toolbar routing)

--- packages/react/src/experimental/Information/Headers/exports.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @deprecated ResourceHeader has moved to @/patterns/ResourceHeader. Import from there instead.
- */
-export * from "../../../patterns/ResourceHeader"
-/**
- * @deprecated SectionHeader has moved to @/patterns/SectionHeader. Import from there instead.
- */
-export * from "../../../patterns/SectionHeader"
