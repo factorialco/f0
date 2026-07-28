d3b935c373ed8c3fe75558c3ee6543f310b89aaf
Zygis Sniurevicius <zygimantas.sniurevicius@factorial.co>
2026-07-28 21:54:51 +0200

    publish: analytics-dashboard-controlled-filters artifact

--- packages/react-native/src/components/F0Button/index.ts
+++
@@ -1,7 +0,0 @@
-export { F0Button } from "./F0Button"
-export { BUTTON_VARIANTS, BUTTON_SIZES } from "./F0Button.types"
-export type {
-  F0ButtonProps,
-  F0ButtonVariant,
-  F0ButtonSize,
-} from "./F0Button.types"
--- packages/react/src/experimental/Forms/Fields/Input/internal.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * @deprecated Moved to `@/components/F0TextInput/internal`.
- * Re-exported here for backwards compatibility. Will be removed in a
- * future major.
- *
- * @removeIn 2.0.0
- */
-export * from "@/components/F0TextInput/internal"
