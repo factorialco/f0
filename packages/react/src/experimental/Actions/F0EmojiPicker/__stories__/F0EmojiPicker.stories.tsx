bae1be006a900c09365777679ab09b191a7e0e01
Raúl Sigüenza Sánchez <raul.siguenza@factorial.co>
2026-08-04 20:15:13 +0200

    docs(emoji): make default story clearable
    Implemented-with: factorial-f0

--- packages/react/src/experimental/Actions/F0EmojiPicker/__stories__/F0EmojiPicker.stories.tsx
+++ packages/react/src/experimental/Actions/F0EmojiPicker/__stories__/F0EmojiPicker.stories.tsx
@@ -64,7 +64,11 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {}
+export const Default: Story = {
+  args: {
+    clearable: true,
+  },
+}
 
 const ControlledExample = () => {
   const [emoji, setEmoji] = useState<string | null>("💬")
